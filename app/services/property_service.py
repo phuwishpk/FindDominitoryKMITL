74c00503f7d556c1ebe6b3f69f445da6454d3d9d
Phinyapat2005 <inging161248@gmail.com>
2025-09-27 23:16:13 +0700

    Added update_property_amenities function to update amenities

--- app/services/property_service.py
+++ app/services/property_service.py
@@ -8,28 +8,22 @@ class PropertyService:
     def create(self, owner_id: int, data: dict) -> Property:
         amenity_codes = data.pop('amenities', [])
         prop = Property(owner_id=owner_id, **data)
-
         if amenity_codes:
             amenities = Amenity.query.filter(Amenity.code.in_(amenity_codes)).all()
             prop.amenities = amenities
-        
         return self.repo.add(prop)
 
     def update(self, owner_id: int, prop_id: int, data: dict):
         prop = self.repo.get(prop_id)
         if not prop or prop.owner_id != owner_id:
             return None
-        
         amenity_codes = data.pop('amenities', [])
-
         for k, v in data.items():
             setattr(prop, k, v)
-        
         if amenity_codes:
             amenities = Amenity.query.filter(Amenity.code.in_(amenity_codes)).all()
             prop.amenities = amenities
         else:
             prop.amenities = []
-
         self.repo.save(prop)
         return prop
