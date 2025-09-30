726d44dab0635c9e38759a92a073030bed33c7c4
IamTubbe <67030298@kmitl.ac.th>
2025-09-30 15:50:36 +0700

    redesign form for single-page UX and clarity

--- app/forms/owner.py
+++ app/forms/owner.py
@@ -6,22 +6,17 @@ from wtforms import ValidationError
 ROOM_TYPES = ("studio", "1br", "2br", "other")
 
 class PropertyForm(FlaskForm):
-    # --- ส่วนที่บังคับกรอก ---
     dorm_name = StringField("ชื่อหอ", validators=[DataRequired("กรุณากรอกชื่อหอพัก"), Length(max=120)])
     room_type = StringField("ประเภทห้อง", validators=[DataRequired("กรุณากรอกประเภทห้อง"), Length(max=30)])
     rent_price = IntegerField("ค่าเช่า", validators=[DataRequired("กรุณากรอกค่าเช่า"), NumberRange(min=0)])
-    
-    # --- ส่วนที่ไม่บังคับ แต่ถ้ากรอกต้องถูกรูปแบบ ---
-    contact_phone = StringField("เบอร์โทร", validators=[Optional(), Length(max=20)])
+    contact_phone = StringField("เบอร์โทร", validators=[DataRequired("กรุณากรอกเบอร์โทรติดต่อ"), Length(max=20)])
     line_id = StringField("LINE ID", validators=[Optional(), Length(max=80)])
-    facebook_url = StringField("Facebook", validators=[Optional(), URL(), Length(max=255)])
-
-    water_rate = FloatField("ค่าน้ำ", validators=[Optional(), NumberRange(min=0)])
-    electric_rate = FloatField("ค่าไฟ", validators=[Optional(), NumberRange(min=0)])
-    deposit_amount = IntegerField("เงินประกัน", validators=[Optional(), NumberRange(min=0)])
-
-    lat = FloatField("ละติจูด", validators=[Optional()])
-    lng = FloatField("ลองจิจูด", validators=[Optional()])
+    facebook_url = StringField("Facebook", validators=[Optional(), URL("กรุณากรอก URL ให้ถูกต้อง"), Length(max=255)])
+    water_rate = FloatField("ค่าน้ำ", validators=[DataRequired("กรุณากรอกค่าน้ำ"), NumberRange(min=0)])
+    electric_rate = FloatField("ค่าไฟ", validators=[DataRequired("กรุณากรอกค่าไฟ"), NumberRange(min=0)])
+    deposit_amount = IntegerField("เงินประกัน", validators=[DataRequired("กรุณากรอกเงินประกัน"), NumberRange(min=0)])
+    lat = FloatField("ละติจูด", validators=[DataRequired("กรุณากรอกพิกัดละติจูด")])
+    lng = FloatField("ลองจิจูด", validators=[DataRequired("กรุณากรอกพิกัดลองจิจูด")])
 
     def validate_room_type(self, field):
         if field.data and field.data.lower() not in ROOM_TYPES:
